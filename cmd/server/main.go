a2d070418e36005af0ca9650493dbe1bc4ad721e
Dung Nguyen <dungnguyentien0409@gmail.com>
2026-04-18 18:41:22 +0200

    feat: add graceful shutdown support for HTTP server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
 	"html/template"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/dungnguyentien0409/web-page-analyzer/internal/fetcher"
@@ -13,11 +17,7 @@ import (
 
 func main() {
 	const requestTimeout = 15 * time.Second
-
-	tmpl := template.Must(
-		template.ParseFiles("web/templates/index.html"),
-	)
-
+	tmpl := template.Must(template.ParseFiles("web/templates/index.html"))
 	fetcherSvc := fetcher.NewDefaultFetcher()
 	analyzer := parser.NewDefaultAnalyzer()
 	h := handler.NewHandler(handler.HandlerConfig{
@@ -26,21 +26,30 @@ func main() {
 		Analyzer:       analyzer,
 		RequestTimeout: requestTimeout,
 	})
-
 	http.HandleFunc("/", h.IndexHandler)
 	http.HandleFunc("/analyze", h.AnalyzeHandler)
-
 	http.Handle("/static/",
 		http.StripPrefix("/static/",
 			http.FileServer(http.Dir("web/static")),
 		),
 	)
-
-	log.Println("Server running at http://localhost:8080")
-
-	err := http.ListenAndServe(":8080", nil)
-
-	if err != nil {
-		log.Fatal(err)
+	srv := &http.Server{
+		Addr: ":8080",
 	}
+	go func() {
+		log.Println("Server running at http://localhost:8080")
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutting down server...")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown:", err)
+	}
+	log.Println("Server exiting")
 }
